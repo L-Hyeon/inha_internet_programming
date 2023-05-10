c6a1f2de3f1f5c9e603aaba9ff8a7abb81d87394
L-Hyun <dkh1001@outlook.kr>
2023-05-10 13:29:54 +0900

    Feat Modify Button Atom to style itself with class name

--- src/components/atoms/button/Button.svelte
+++ src/components/atoms/button/Button.svelte
@@ -6,20 +6,17 @@
 	export let height: string = "50px";
 	export let type: number = 1;
 	export let fontSize: string = "medium";
+	export let classNames: string[] = [];
+	export let borderRadius: boolean = true;
 </script>
 
 <button
 	on:click={onClick}
 	style="width:{width};height:{height};"
-	class={[`button-type-${type}`, `font-size-${fontSize}`].join(" ")}
-	><slot /></button
+	class={[
+		`button-type-${type}`,
+		`font-size-${fontSize}`,
+		borderRadius ? "border-radius-10" : "border-radius-none",
+		...classNames,
+	].join(" ")}><slot /></button
 >
-
-<style>
-	button {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		border-radius: 10px;
-	}
-</style>
